18d74f7ed22ceaba61cb209015f2b24a33824b08
thesebonesalone <duikhead2@gmail.com>
2020-09-23 21:51:07 -0500

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,18 +1,30 @@
 class Student
   attr_accessor :id, :name, :grade
-
   def self.new_from_db(row)
     # create a new Student object given a row from the database
-  end
-
-  def self.all
-    # retrieve all the rows from the "Students" database
-    # remember each row should be a new instance of the Student class
+    new_student = self.new
+    new_student.id = row[0]
+    new_student.name = row[1]
+    new_student.grade = row[2]
+    new_student
   end
 
   def self.find_by_name(name)
-    # find the student in the database given a name
-    # return a new instance of the Student class
+    sql = <<-SQL
+    SELECT * FROM students
+    WHERE name = ?
+    LIMIT 1
+    SQL
+    self.build_from_array(DB[:conn].execute(sql, name))[0]
+    
+
+  end
+
+  def self.all
+    sql = <<-SQL
+    SELECT * FROM students
+    SQL
+    self.build_from_array(DB[:conn].execute(sql))
   end
   
   def save
@@ -40,4 +52,41 @@ class Student
     sql = "DROP TABLE IF EXISTS students"
     DB[:conn].execute(sql)
   end
+  def self.all_students_in_grade_9
+    sql = <<-SQL
+    SELECT * FROM students WHERE grade = 9
+    SQL
+    DB[:conn].execute(sql)
+
+  end
+  def self.students_below_12th_grade
+    sql = <<-SQL
+    SELECT * FROM students WHERE grade <= 11
+    SQL
+    self.build_from_array(DB[:conn].execute(sql))
+  end
+  def self.first_X_students_in_grade_10(x)
+    sql = <<-SQL
+    SELECT * FROM students WHERE grade = 10 ORDER BY id LIMIT ?
+    SQL
+    self.build_from_array(DB[:conn].execute(sql, x))
+  end
+  def self.first_student_in_grade_10
+    self.first_X_students_in_grade_10(1)[0]
+  end
+  def self.all_students_in_grade_X(x)
+    sql = <<-SQL
+    SELECT * FROM students WHERE grade = ?
+    SQL
+    self.build_from_array(DB[:conn].execute(sql, x))
+  end
+  def self.build_from_array(array)
+    array.map{|value| self.new_from_db(value)}
+  end
+
+
+
+
+
+
 end
